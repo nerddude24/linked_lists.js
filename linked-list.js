f1615358787c8f9bd8f9057e8a7ddef885f3b2a1
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-08-28 14:17:50 +0100

    Add node and linked list class (forgot to commit frequently)

---
+++ linked-list.js
@@ -0,0 +1,146 @@
+class Node {
+	constructor(val = null, next = null) {
+		this.val = val;
+		this.next = next;
+	}
+}
+
+class LinkedList {
+	constructor(headVal) {
+		const node = new Node(headVal);
+		this._head = node;
+		this._tail = node;
+	}
+
+	append(val) {
+		const node = new Node(val);
+		this._tail.next = node;
+		this._tail = node;
+	}
+
+	prepend(val) {
+		const node = new Node(val);
+		node.next = this._head;
+		this._head = node;
+	}
+
+	size() {
+		let current = this._head;
+		let numOfNodes = 0;
+
+		while (current != null) {
+			numOfNodes++;
+			current = current.next;
+		}
+
+		return numOfNodes;
+	}
+
+	head() {
+		return this._head;
+	}
+
+	tail() {
+		return this._tail;
+	}
+
+	at(index) {
+		let current = this._head;
+		let currentIndex = 0;
+
+		while (current != null) {
+			if (currentIndex == index) return current;
+			current = current.next;
+			currentIndex++;
+		}
+
+		return null;
+	}
+
+	pop() {
+		let current = this._head;
+
+		// keep moving until you reach the node that's just before the tail.
+		while (current.next != this._tail) {
+			current = current.next;
+		}
+
+		// current is now the node that's just before the tail.
+		const poppedNode = this._tail;
+		current.next = null;
+		this._tail = current;
+
+		return poppedNode;
+	}
+
+	contains(target) {
+		let current = this._head;
+
+		while (current != null) {
+			if (current.val === target) return true;
+			current = current.next;
+		}
+
+		return false;
+	}
+
+	find(target) {
+		let current = this._head;
+		let currentIndex = 0;
+
+		while (current != null) {
+			if (current.val === target) return currentIndex;
+			current = current.next;
+			currentIndex++;
+		}
+
+		return -1;
+	}
+
+	toString() {
+		let current = this._head;
+		let str = "";
+
+		while (current != null) {
+			str += `( ${current.val} ) -> `;
+			current = current.next;
+		}
+
+		// last node always points to null, so add it to str
+		str += "null";
+
+		return str;
+	}
+
+	insertAt(val, index) {
+		// insert at head
+		if (index == 0) {
+			const node = new Node(val);
+			node.next = this._head;
+			this._head = node;
+
+			return;
+		}
+
+		// else if index is not at head
+		let current = this._head;
+		let prev = null;
+		let currentIndex = 0;
+
+		while (current != null) {
+			if (currentIndex == index) {
+				const node = new Node(val);
+				prev.next = node;
+				node.next = current;
+				return;
+			}
+			prev = current;
+			current = current.next;
+			currentIndex++;
+		}
+
+		throw new Error("Error: Tried to insert at invalid index (out of range)");
+	}
+}
+
+export { Node, LinkedList };
